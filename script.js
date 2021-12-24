deb8b2599ed45ce53d0fa60bada77861f6fdca8b
Asadbek Nosirjonov5566 <a.nosirjonov@uic.group>
2021-12-24 19:24:03 +0500

    finish

--- script.js
+++ script.js
@@ -1,3 +1,81 @@
+const api = {
+    key: '14c12046118a3fe1041892a31e4fed2e',
+    baseURL: 'https://api.openweathermap.org/data/2.5/',
+}
+
+const search = document.querySelector('.search');
+search.addEventListener('keypress', setQuery);
+
+function setQuery(e) {
+    if (e.keyCode = 13) {
+        getResults(search.value);
+        // console.log(search.value); 
+    }
+}
+
+function getResults(quaerty) {
+    fetch(`${api.baseURL}weather?q=${quaerty}&APPID=${api.key}&units=metric`)
+        .then(weather => {
+            return weather.json();
+        })
+        .then(displayResults);
+}
+
+function displayResults(weather) {
+    console.log(weather);
+    const city = document.querySelector('.location .city');
+    city.innerHTML = `${weather.name}, ${weather.sys.country}`;
+    let now = new Date();
+    const date = document.querySelector('.location .date'); 
+    date.innerHTML = dateBuilder(now);
+
+    let temp = document.querySelector('.temp');
+    temp.innerHTML = `${Math.round(weather.main.temp) + `°C`}`;
+
+    let weather_el = document.querySelector('.weather');
+    weather_el.innerHTML = `${weather.weather[0].main}`;
+    let hilow = document.querySelector('.hi-low');
+    hilow.innerHTML = `${Math.round(weather.main.temp_min) + `°C / ${Math.round(weather.main.temp_max) + `°C`}`}`;
+}
+
+
+function dateBuilder(d) {
+    let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+    let day = days[d.getDay()]
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // let arr = [4, 5, 6, 7, 8, 9, 10];
 // arr.forEach((element) => {
 //     console.log(Math.max(element));
